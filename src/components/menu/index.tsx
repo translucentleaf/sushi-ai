136950281b9684a0c18d3a6e25b535be52df95e4
Tom Dong <translucentleaf@protonmail.com>
2020-01-08 10:43:06 -0600

    Added actual images for the menu page icons

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styled, { AnyStyledComponent } from "styled-components";
 import { Link } from "react-router-dom";
-import menuBanner from "../../assets/images/banners/menu_banner.jpg";
 
 import { Banner } from "../banner/index";
 import { Footer } from "../footer/index";
+
+import menuBanner from "../../assets/images/banners/menu_banner.jpg";
+import regularMenuImg from "../../assets/images/misc/regular_menu_img.jpg";
+import lunchMenuImg from "../../assets/images/misc/lunch_menu_img.jpg";
+import dinnerMenuImg from "../../assets/images/misc/dinner_menu_img.jpg";
 
 const MenuIcon: AnyStyledComponent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
   height: 250px;
   max-width: 150px;
   text-align: center;
@@ -25,7 +30,9 @@ const MenuText: AnyStyledComponent = styled(Link)`
   justify-content: center;
   align-items: center;
 
-  background-color: lightblue;
+  background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)),
+    url(${(props: IconProps): string => props.img}) no-repeat center;
+  background-size: cover;
   border-radius: 10px;
   color: white;
   margin: 40px 40px 40px 40px;
@@ -36,13 +43,14 @@ const MenuText: AnyStyledComponent = styled(Link)`
 interface IconProps {
   name: string;
   link: string;
+  img: string;
 }
 
 const MenuPageIcon: React.FC<IconProps> = (props): JSX.Element => {
-  const { name, link } = props;
+  const { name, link, img } = props;
 
   return (
-    <MenuText to={link}>
+    <MenuText to={link} img={img}>
       <MenuIcon>
         <h1>{name}</h1>
       </MenuIcon>
@@ -77,9 +85,21 @@ const Menu: React.FC = () => {
     <MenuWrapper>
       <Banner image={menuBanner} text="MENU" height="350px" position="center" />
       <MenuGroup>
-        <MenuPageIcon name="Regular Menu" link="/regular-menu" />
-        <MenuPageIcon name="Lunch All You Can Eat" link="/lunch-menu" />
-        <MenuPageIcon name="Dinner All You Can Eat" link="/dinner-menu" />
+        <MenuPageIcon
+          name="Regular Menu"
+          link="/regular-menu"
+          img={regularMenuImg}
+        />
+        <MenuPageIcon
+          name="Lunch All You Can Eat"
+          link="/lunch-menu"
+          img={lunchMenuImg}
+        />
+        <MenuPageIcon
+          name="Dinner All You Can Eat"
+          link="/dinner-menu"
+          img={dinnerMenuImg}
+        />
       </MenuGroup>
       <Footer />
     </MenuWrapper>
